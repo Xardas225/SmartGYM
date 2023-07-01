bde9850e2a769e1ee787e82f133913cfa7ba4b76
Alexander Derevkov <78501617+Xardas225@users.noreply.github.com>
2023-07-01 18:29:19 +0300

    change structure folder in butrition, add baseselect comp

---
+++ components/TheBaseSelect.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps({
+  options: Array
+});
+</script>
+
+<template>
+  <select class="form-select">
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+  </select>
+</template>
---
+++ pages/nutrition/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="spinner" v-if="cardData?.length == 0"></div>
+
+    <div v-else>
+      <div class="row text-center my-4">
+        <h2>Питание</h2>
+      </div>
+      <div class="row">
+        <article class="col-3"></article>
+        <main class="col-9">
+          <div class="row">
+            <TheCard
+              class="col-4"
+              v-for="item in cardData"
+              :key="item.id"
+              :="item"
+            />
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: "Питание",
+});
+
+const { data: cardData } = await useAsyncData("food", () =>
+  $fetch("/api/food")
+);
+</script>
